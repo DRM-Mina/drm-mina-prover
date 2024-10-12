36574474948e7676661027d223e566bf5e300792
kadirchan <kadircanbozkurt1905@gmail.com>
2024-10-12 17:11:45 +0300

    update gameProver add offchain state getter

--- server.ts
+++ server.ts
@@ -1,31 +1,66 @@
-import { DeviceSession, DeviceSessionInput, Identifiers } from "drm-mina-contracts";
+import { DRM, offchainState } from "drm-mina-contracts/build/src/DRM.js";
+import { Identifiers } from "drm-mina-contracts/build/src/lib/DeviceIdentifier.js";
+import {
+    DeviceSession,
+    DeviceSessionInput,
+} from "drm-mina-contracts/build/src/lib/DeviceSessionProof.js";
 import express from "express";
-import { UInt64 } from "o1js";
+import { fetchAccount, Field, Mina, PublicKey, UInt64 } from "o1js";
 
 const apiEndpoint =
     process.env.NODE_ENV === "production" ? "https://drm-mina.com" : "http://localhost:3333/";
 
-let isCompiled = false;
+const drmAddress = process.argv[2];
+const gameTokenAddress = process.argv[3];
+
+console.log("DRM address", drmAddress);
+console.log("Game token address", gameTokenAddress);
+if (!drmAddress) {
+    console.error("DRM address is required");
+    process.exit(1);
+}
+
+if (!gameTokenAddress) {
+    console.error("Game token address is required");
+    process.exit(1);
+}
+
+let isDeviceSessionCompiled = false;
+let isOffChainStateCompiled = false;
+
 (async () => {
-    console.log("Compiling DeviceSession");
+    console.time("Compiling DeviceSession");
     await DeviceSession.compile();
-    isCompiled = true;
-    console.log("DeviceSession compiled");
+    isDeviceSessionCompiled = true;
+    console.timeEnd("Compiling DeviceSession");
+
+    const Network = Mina.Network({
+        mina: "https://api.minascan.io/node/devnet/v1/graphql",
+        archive: "https://api.minascan.io/archive/devnet/v1/graphql",
+    });
+    Mina.setActiveInstance(Network);
+
+    console.time("Compile OffchainState");
+    const drmInstance = new DRM(PublicKey.fromBase58(drmAddress));
+    offchainState.setContractInstance(drmInstance);
+    await offchainState.compile();
+    isOffChainStateCompiled = true;
+    console.timeEnd("Compile OffchainState");
 })();
 
 const app = express();
 app.use(express.json());
 
 app.post("/", async (req, res) => {
-    if (!isCompiled) {
+    if (!isDeviceSessionCompiled) {
         res.status(102).send("DeviceSession not compiled yet");
         return;
     }
     try {
-        const { rawIdentifiers, currentSession, newSession, gameId } = req.body;
+        const { rawIdentifiers, currentSession, newSession } = req.body;
         const identifiers = Identifiers.fromRaw(rawIdentifiers);
         const publicInput = new DeviceSessionInput({
-            gameId: UInt64.from(gameId),
+            gameToken: PublicKey.fromBase58(gameTokenAddress),
             currentSessionKey: UInt64.from(currentSession),
             newSessionKey: UInt64.from(newSession),
         });
@@ -45,6 +80,37 @@ app.post("/", async (req, res) => {
             throw new Error(`Failed to submit session: ${response.status}`);
         }
         res.status(200).send("Transaction sent");
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Transaction failed");
+    }
+});
+
+app.post("/current-session", async (req, res) => {
+    if (!isOffChainStateCompiled) {
+        res.status(102).send("OffChainState not compiled yet");
+        return;
+    }
+
+    try {
+        const { deviceHash } = req.body;
+
+        console.log(deviceHash);
+
+        await fetchAccount({
+            publicKey: PublicKey.fromBase58(drmAddress),
+        });
+
+        await Mina.fetchActions(PublicKey.fromBase58(drmAddress));
+
+        const currentSession = await offchainState.fields.sessions.get(Field.from(deviceHash));
+        if (!currentSession.isSome.toBoolean()) {
+            console.log("Current session not found");
+            res.status(200).send({ currentSession: 0 });
+        } else {
+            console.log("Current session", currentSession.value.toString());
+            res.status(200).send({ currentSession: currentSession.value.toString() });
+        }
     } catch (e) {
         console.error(e);
         res.status(500).send("Transaction failed");
